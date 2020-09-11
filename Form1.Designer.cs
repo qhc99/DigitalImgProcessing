c61b73e59922370dc27b2709982ad60d4ceee364
nathan <1136026744@qq.com>
2020-09-11 11:11:14 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace opencv
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(42, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "加载图片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(187, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 320);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(513, 37);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 320);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(453, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "清除此行图片";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(42, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 25);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "灰度化";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(305, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "第   行 第   列";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(638, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "第   行 第   列";
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("Microsoft YaHei UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(854, 166);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(45, 40);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "↑";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Font = new System.Drawing.Font("Microsoft YaHei UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(854, 221);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(44, 44);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "↓";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ImgToolBox";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Splitter splitter2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Splitter splitter3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using OpenCvSharp;
+using Size = OpenCvSharp.Size;
+
+namespace opencv
+{
+    public partial class Form1 : Form
+    {
+        private readonly List<List<Mat>> _workingProcess = new List<List<Mat>>();
+        private readonly List<List<Mat>> _resultProcess = new List<List<Mat>>();
+        private readonly List<PictureBoxIndices> _processesPictureIndices = new List<PictureBoxIndices>();
+        private int _currentProcessIndex = 0;
+
+        internal sealed class PictureBoxIndices
+        {
+            public int LeftIndex;
+            public int RightIndex;
+
+            public PictureBoxIndices(int l, int r)
+            {
+                LeftIndex = l;
+                RightIndex = r;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 显示空图片
+        /// </summary>
+        /// <param name="pb"></param>
+        private void LoadNoneImg(PictureBox pb)
+        {
+            pb.Image = Image.FromFile(Directory.GetCurrentDirectory() + "..\\..\\..\\..\\Resources\\timg.jpg");
+        }
+
+        private List<Mat> WorkingImages => _workingProcess[_currentProcessIndex];
+
+        private List<Mat> ResultImages => _resultProcess[_currentProcessIndex];
+
+        private PictureBoxIndices CurrentPictureBoxIndices =>
+            _processesPictureIndices[_currentProcessIndex];
+
+        private Mat WorkingLeftImg =>
+            WorkingImages[CurrentPictureBoxIndices.LeftIndex];
+
+        private Mat WorkingRightImg =>
+            WorkingImages[CurrentPictureBoxIndices.RightIndex];
+
+        private Mat ResultLeftImg =>
+            ResultImages[CurrentPictureBoxIndices.LeftIndex];
+
+        private Mat ResultRightImg =>
+            ResultImages[CurrentPictureBoxIndices.RightIndex];
+
+        /// <summary>
+        /// 显示Mat图片
+        /// </summary>
+        /// <param name="pb"></param>
+        /// <param name="m"></param>
+        private void ShowMat(PictureBox pb, Mat m)
+        {
+            pb.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(m);
+            if (pictureBox1 == pb)
+            {
+                label1.Text = $@"第 {_currentProcessIndex+1} 行 第 {CurrentPictureBoxIndices.LeftIndex + 1} 列";
+            }
+            else
+            {
+                label2.Text = $@"第 {_currentProcessIndex + 1} 行 第 {CurrentPictureBoxIndices.RightIndex + 1} 列";
+            }
+        }
+        
+        private static Tuple<int, int> ComputeSize(int imgRow, int imgCol, int boxRow, int boxCol)
+        {
+            if (imgRow <= boxRow && imgCol <= boxCol)
+            {
+                return new Tuple<int, int>(imgRow, imgCol);
+            }
+            else
+            {
+                double ratio = ((double)imgRow) / imgCol;
+                //newRow/newCol = imgRow/imgCol
+                if (imgRow > boxRow)
+                {
+                    return new Tuple<int, int>(boxRow, (int)(boxRow / ratio));
+                }
+                else
+                {
+                    return new Tuple<int, int>((int)(ratio * boxCol), boxCol);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 计算放缩后的尺寸
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        private Size GetBoxFittedSizeFromImg(Mat image)
+        {
+            var (row, col) = ComputeSize(image.Rows, image.Cols,
+                pictureBox1.Size.Height, pictureBox2.Size.Width);
+            return new Size(row, col);
+        }
+
+        /// <summary>
+        /// 加载图片后恢复所有按钮
+        /// </summary>
+        private void EnableAllButtons()
+        {
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+        }
+
+        /// <summary>
+        /// 加载图片到两个工作序列,变换显示
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                _resultProcess.Add(new List<Mat>());
+                _workingProcess.Add(new List<Mat>());
+                _processesPictureIndices.Add(new PictureBoxIndices(0,0));
+                if (_currentProcessIndex != 0) _currentProcessIndex++;
+                var originImg = new Mat(openFileDialog1.FileName);
+                
+                ResultImages.Add(originImg);
+                
+                Mat resizedImg = originImg.Resize(GetBoxFittedSizeFromImg(originImg), 0, 0, InterpolationFlags.Cubic);
+                
+                _workingProcess[_currentProcessIndex].Add(resizedImg);
+                ShowMat(pictureBox1, resizedImg);
+
+                EnableAllButtons();
+
+            }
+        }
+
+        /// <summary>
+        /// 清除此行图片
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoadNoneImg(pictureBox1);
+            LoadNoneImg(pictureBox2);
+            _workingProcess.RemoveAt(_currentProcessIndex);
+            _resultProcess.RemoveAt(_currentProcessIndex);
+            _processesPictureIndices.RemoveAt(_currentProcessIndex);
+        }
+
+        /// <summary>
+        /// 灰度化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var indices = CurrentPictureBoxIndices;
+            Mat originImg, resOriginImg, grayImg, resGrayImg;
+            if (indices.LeftIndex == indices.RightIndex)
+            {
+                originImg = WorkingLeftImg;
+                resOriginImg = ResultLeftImg;
+            }
+            else
+            {
+                originImg = WorkingRightImg;
+                resOriginImg = WorkingRightImg;
+            }
+
+            try
+            {
+                grayImg = originImg.CvtColor(ColorConversionCodes.BGR2GRAY, 1);
+                resGrayImg = resOriginImg.CvtColor(ColorConversionCodes.BGR2GRAY, 1);
+            }
+            catch (OpenCVException)
+            {
+                MessageBox.Show(@"不能重复灰度化图像");
+                return;
+            }
+
+            WorkingImages.Add(grayImg);
+            ResultImages.Add(resGrayImg);
+            if (indices.RightIndex != 0) indices.LeftIndex++;
+            indices.RightIndex++;
+
+            ShowMat(pictureBox1, WorkingLeftImg);
+            ShowMat(pictureBox2, WorkingRightImg);
+        }
+
+        /// <summary>
+        /// 上移一行
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 下移一行
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
